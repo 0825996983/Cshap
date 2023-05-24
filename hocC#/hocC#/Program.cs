6deb0880355165158c63493e7a66a56153980f55
0825996983 <doquangbien3172003@gmail.com>
2023-05-24 09:20:54 +0700

    C#

---
+++ hocC#/hocC#/Program.cs
@@ -0,0 +1,10 @@
+﻿using hocC_;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        person bl = new person();
+        bl.MakeSound();
+    }
+}
---
+++ hocC#/hocC#/people.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hocC_
+{
+    abstract class people
+    {
+        public abstract void MakeSound();
+
+
+        public void run()
+        {
+            Console.WriteLine("dog ");
+        }
+     
+
+    }
+      class person : people
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("gau gau");    
+        }
+    }
+
+ }
+
+
